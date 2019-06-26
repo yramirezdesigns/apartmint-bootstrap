0607f2128b3d1d2a2a3a3f2a8fdbfdc2dd28ca86
Sandy Liao <sandy.l45@gmail.com>
2019-06-26 00:38:32 -0400

    workign button

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
 import React from "react";
 import logo from "./ApartmintLogoSmall.png";
 import "./App.css";
-import { Button, ButtonToolbar, ButtonGroup, Image } from "react-bootstrap";
-import backDrop from "./assets/bg-1.jpg";
+// import { Button, ButtonToolbar, ButtonGroup, Image } from "react-bootstrap";
+// import backDrop from "./assets/bg-1.jpg";
+// import Modal from "./components/Modal";
+import Login from "./components/Login";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Image className="backDrop" alt="Background" src={backDrop} fluid />
-        {/* <img className="backDrop" alt="Background" src={backDrop} /> */}
-        <ButtonToolbar>
-          <ButtonGroup className="mr-2" aria-label="First group">
-            <Button variant="info" className="loginBut">
-              Login
-            </Button>
-          </ButtonGroup>
-          <ButtonGroup className="mr-2" aria-label="First group">
-            <Button variant="info" className="registerBut">
-              Register
-            </Button>
-          </ButtonGroup>
-        </ButtonToolbar>
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <Login />
       </header>
     </div>
   );
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Button extends React.Component {
+  render() {
+    return <button onClick={() => this.props.onClickHandler()}>{this.props.buttonText}</button>
+  }
+}
