d2a6b0ea94b9033d7c5d65776f0bbde833eac5ad
yoli <yramirezdesigns@gmail.com>
2019-06-25 21:04:29 -0400

    bootstrap installed, and two buttons not linked

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "./ApartmintLogoSmall.png";
+import "./App.css";
+import { Button, ButtonToolbar, ButtonGroup, Image } from "react-bootstrap";
+import backDrop from "./assets/bg-1.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Image className="backDrop" alt="Background" src={backDrop} fluid />
+        {/* <img className="backDrop" alt="Background" src={backDrop} /> */}
+        <ButtonToolbar>
+          <ButtonGroup className="mr-2" aria-label="First group">
+            <Button variant="info" className="loginBut">
+              Login
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup className="mr-2" aria-label="First group">
+            <Button variant="info" className="registerBut">
+              Register
+            </Button>
+          </ButtonGroup>
+        </ButtonToolbar>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
